25fa5fcb545dfc302ebc7c5f7470d60f1d161ff0
johngova <gova.vardhan619@gmail.com>
2018-03-05 12:16:19 +0530

    this is my vardhan project commit

---
+++ vardhan12/core/src/main/java/john/core/models/Simple11.java
@@ -0,0 +1,14 @@
+package john.core.models;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+public class Simple11 extends WCMUsePojo{
+
+	@Override
+	public void activate() throws Exception {
+		// TODO Auto-generated method stub
+
+		System.out.println("iam in maven");
+	}
+
+}
